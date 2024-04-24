eb540c5c1611f95e10db4c28e9c6f83d7d7ceae1
Matt Blum <matt@nationalmachine.co>
2024-04-24 10:08:16 -0500

    updates that may or may not have been necessary

--- deploy_sdc.sh
+++ deploy_sdc.sh
@@ -1,118 +1,149 @@
 #!/bin/bash
 
-# The sdc user's home directory
-sdc_home=/usr/local/cdo
+usage() {
+  echo "usage: source deploy_sdc.sh [bootstrap data]" >&2
+  echo "example: source deploy_sdc.sh Q0RPX1RPS0VOPSJleU...Y29fYWFoYWNrbmUtU0RDLTQiCg==" >&2
+}
+
+# Make sure the bootstrap.sh prerequisite packages are installed
+validate_packages() {
+  for package in net-tools awscli
+  do
+    if [ ! "$(sudo dpkg -l | awk '/'"$package"'/ {print }'|wc -l)" -ge 1 ]; then
+      echo "$package is required for the CDO SDC bootstrap script and is not installed."
+      echo "Installing $package"
+      sudo apt-get install "$package" -y
+    fi
+  done
+}
+
+make_sdc_user() {
+  sdc_home_default=/home/sdc
+  read -p "Home dir for the sdc user [$sdc_home_default]:" sdc_home
+  sdc_home=${sdc_home:-$sdc_home_default}
+  mkdir -p "$sdc_home"
+
+  echo "Creating user: sdc"
+  sudo adduser --gecos "" --disabled-password sdc --home "$sdc_home"
+
+  # Set permissions for sdc user's home dir
+  sudo chown sdc:sdc "$sdc_home"
+}
+
+# Create sdc user if it does not exist and set home directory
+configure_users_and_groups() {
+  if id sdc >/dev/null 2>&1; then
+      sdc_home=$( getent passwd "sdc" | cut -d: -f6 )
+      echo "Found existing user: sdc with a home dir location of $sdc_home"
+  else
+      make_sdc_user
+  fi
+
+  # Create the docker group if it does not exist
+  if [ $(getent group docker) ]; then
+      echo "Found existing group: docker"
+  else
+      echo "Creating group: docker"
+      sudo groupadd docker
+  fi
+
+  # Add the sdc user to the docker group
+  echo "Adding the sdc user to the group docker"
+  if [ ! $(getent group docker | grep -qw "sdc")]; then
+      sudo usermod -aG docker sdc
+  fi
+}
+
+configure_docker() {
+  # Check for the docker daemon.json file
+  echo "Checking to see if the file daemon.json file exists in /etc/docker"
+  daemon_json='{"live-restore": true, "group": "docker"}'
+
+  if [ -f /etc/docker/daemon.json ]; then
+    echo "/etc/docker/daemon.json exists. Please make sure the following parameters are in /etc/docker/daemon.json"
+    echo $daemon_json
+  else
+    echo "Writing file /etc/docker/daemon.json"
+    echo ${daemon_json} > daemon.json
+    sudo cp daemon.json /etc/docker/daemon.json
+    rm daemon.json
+  fi
+
+  # Restart the docker daemon
+  echo "Restarting docker daemon"
+  sudo systemctl restart docker
+  echo "Docker status after restart:"
+  echo $(sudo systemctl status docker | grep 'Active')
+}
+
+# Decode SDC bootstrap data and extract the needed pieces
+bootstrap() {
+  echo "Decoding the bootstrap data..."
+  decoded_bootstrap=$(echo "$1" | base64 --decode)
+
+  # Write env vars to file for the sdc and also load the vars
+  printf '%s\n' ${decoded_bootstrap} > sdcenv
+  source sdcenv
+  rm sdcenv
+
+  # Download the bootstrap file from CDO
+  echo Downloading CDO Bootstrap File
+  sudo curl --output "$sdc_home/${CDO_BOOTSTRAP_URL##*/}" --header "Authorization: Bearer ${CDO_TOKEN}" "$CDO_BOOTSTRAP_URL"
+
+  # Untarring CDO Bootstrap file
+  sudo tar xzvf "$sdc_home/${CDO_BOOTSTRAP_URL##*/}" --directory "$sdc_home"
+
+  # Remove the tar file
+  sudo rm "$sdc_home/${CDO_BOOTSTRAP_URL##*/}"
+
+  # chown the new files to sdc user
+  sudo chown --recursive sdc:sdc "$sdc_home"
+}
+
+# Final check for success and exit
+validate_install() {
+  if sudo test -f "${sdc_home}/bootstrap/bootstrap.sh"; then
+    echo
+    echo "***********************************************************************************************"
+    echo "SDC pre-configuration scripts appears to have completed successfully."
+    echo "Running the CDO SDC Bootstrap script to finsh the deployment process:"
+    
+    # Export vars to pass into the bootstrap script via sudo
+    export CDO_TOKEN=$CDO_TOKEN
+    export CDO_DOMAIN=$CDO_DOMAIN
+    export CDO_TENANT=$CDO_TENANT
+    export CDO_BOOTSTRAP_URL=$CDO_BOOTSTRAP_URL
+    export HOME=/usr/local/cdo
+    
+    # Patch common.sh to use absoulte path for needed files...
+    sudo -Eu sdc sed -i '6s/.*/env_dir=$(dirname $(readlink -f $0))/' /usr/local/cdo/bootstrap/common.sh
+    
+    # Execute bootstrap script
+    sudo -Eu sdc ${sdc_home}/bootstrap/bootstrap.sh
+    echo "Check that the docker container 'sdc_prod:ubuntu' is up and running..."
+    sudo -Eu sdc docker ps
+    echo "If the docker container is up and running, the status of the SDC should go to 'Active' in the CDO Event Connectors panel."
+  else
+    echo "***********************************************************************************************" >&2
+    echo "Something went wrong with the pre-configuration script." >&2
+    echo "Post your issue in [github](https://github.com/CiscoDevNet/cdo-deploy-sdc/issues) and include the output from this script with the debug option enabled being sure ** NOT to post any passwords or API keys**!" >&2
+    echo "bash -x sdc-pre-config.sh" >&2
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >&2
+  fi
+}
 
 # We need the b64 bootstrap payload from CDO as a parameter for the script
 if [ "$#" -ne 1 ]; then
-    echo "usage: source deploy_sdc.sh [bootstrap data]" >&2
-    echo "example: source deploy_sdc.sh Q0RPX1RPS0VOPSJleU...Y29fYWFoYWNrbmUtU0RDLTQiCg==" >&2
+    usage
     exit
 fi
 
-# Make sure the bootstrap.sh prerequisite packages are installed
-for package in net-tools awscli
-do
-  if [ ! "$(sudo dpkg -l | awk '/'"$package"'/ {print }'|wc -l)" -ge 1 ]; then
-    echo "$package is required for the CDO SDC bootstrap script and is not installed."
-    echo "Installing $package"
-    sudo apt-get install "$package" -y
-  fi
-done
+validate_packages
 
-# Create sdc user if it does not exist
-if id sdc >/dev/null 2>&1; then
-    echo "Found existing user: sdc"
-else
-    echo "Creating user: sdc"
-    sudo adduser --gecos "" --disabled-password sdc --home "$sdc_home"
-fi
+configure_users_and_groups
 
-# Make sure the sdc user's home dir exists
-if [ ! -d /usr/local/cdo ]; then
-    sudo mkdir "$sdc_home"
-    sudo chown sdc:sdc "$sdc_home"
-fi
+configure_docker
 
-# Create the docker group if it does not exist
-if [ $(getent group docker) ]; then
-    echo "Found existing group: docker"
-else
-    echo "Creating group: docker"
-    sudo groupadd docker
-fi
+bootstrap
 
-# Add the sdc user to the docker group
-echo "Adding the sdc user to the group docker"
-if [ ! $(getent group docker | grep -qw "sdc")]; then
-    sudo usermod -aG docker sdc
-fi
-
-# Check for the docker daemon.json file
-echo "Checking to see if the file daemon.json file exists in /etc/docker"
-daemon_json='{"live-restore": true, "group": "docker"}'
-
-if [ -f /etc/docker/daemon.json ]; then
-  echo "/etc/docker/daemon.json exists. Please make sure the following parameters are in /etc/docker/daemon.json"
-  echo $daemon_json
-else
-  echo "Writing file /etc/docker/daemon.json"
-  echo ${daemon_json} > daemon.json
-  sudo cp daemon.json /etc/docker/daemon.json
-  rm daemon.json
-fi
-
-# Restart the docker daemon
-echo "Restarting docker daemon"
-sudo systemctl restart docker
-echo "Docker status after restart:"
-echo $(sudo systemctl status docker | grep 'Active')
-
-# Decode bootstrap data and extract the needed pieces
-echo "Decoding the bootstrap data..."
-decoded_bootstrap=$(echo "$1" | base64 --decode)
-
-# Write env vars to file for the sdc and also load the vars
-printf '%s\n' ${decoded_bootstrap} > sdcenv
-source sdcenv
-rm sdcenv
-
-# Download the bootstrap file from CDO
-echo Downloading CDO Bootstrap File
-sudo curl --output "$sdc_home/${CDO_BOOTSTRAP_URL##*/}" --header "Authorization: Bearer ${CDO_TOKEN}" "$CDO_BOOTSTRAP_URL"
-
-# Untarring CDO Bootstrap file
-sudo tar xzvf "$sdc_home/${CDO_BOOTSTRAP_URL##*/}" --directory "$sdc_home"
-
-# Remove the tar file
-sudo rm "$sdc_home/${CDO_BOOTSTRAP_URL##*/}"
-
-# chown the new files to sdc user
-sudo chown --recursive sdc:sdc "$sdc_home"
-
-# Final check for success and exit
-if sudo test -f "${sdc_home}/bootstrap/bootstrap.sh"; then
-  echo
-  echo "***********************************************************************************************"
-  echo "SDC pre-configuration scripts appears to have completed successfully."
-  echo "Running the CDO SDC Bootstrap script to finsh the deployment process:"
-  # Export vars to pass into the bootstrap script via sudo
-  export CDO_TOKEN=$CDO_TOKEN
-  export CDO_DOMAIN=$CDO_DOMAIN
-  export CDO_TENANT=$CDO_TENANT
-  export CDO_BOOTSTRAP_URL=$CDO_BOOTSTRAP_URL
-  export HOME=/usr/local/cdo
-  # Patch common.sh to use absoulte path for needed files...
-  sudo -Eu sdc sed -i '6s/.*/env_dir=$(dirname $(readlink -f $0))/' /usr/local/cdo/bootstrap/common.sh
-  # Execute bootstrap script
-  sudo -Eu sdc ${sdc_home}/bootstrap/bootstrap.sh
-  echo "Check that the docker container 'sdc_prod:ubuntu' is up and running..."
-  sudo -Eu sdc docker ps
-  echo "If the docker container is up and running, the status of the SDC should go to 'Active' in the CDO Event Connectors panel."
-else
-  echo "***********************************************************************************************" >&2
-  echo "Something went wrong with the pre-configuration script." >&2
-  echo "Post your issue in [github](https://github.com/CiscoDevNet/cdo-deploy-sdc/issues) and include the output from this script with the debug option enabled being sure ** NOT to post any passwords or API keys**!" >&2
-  echo "bash -x sdc-pre-config.sh" >&2
-  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >&2
-fi
+validate_install
--- install_docker.sh
+++ install_docker.sh
@@ -2,69 +2,84 @@
 DOCKER_GPG="https://download.docker.com/linux/ubuntu/gpg"
 DOCKER_APT="https://download.docker.com/linux/ubuntu"
 
-source /etc/lsb-release
-# Detect Ubuntu release - We only support the latest LTS releases
-if [[ ${DISTRIB_CODENAME} == "jammy" || ${DISTRIB_CODENAME} == "focal" ]]; then
-    echo ${DISTRIB_CODENAME}
-else
-    echo "${DISTRIB_CODENAME} is not supported by this installation script."
-    exit 1
-fi
-echo "Detected Ubuntu version ${DISTRIB_RELEASE}: ${DISTRIB_CODENAME}"
+# Detect Ubuntu release - We only support the latest Ubuntu LTS releases
+detect_os() {
+    source /etc/lsb-release
 
-# Detect x86 cpu architecture or arm64 cpu architecture
-if [[ $(arch) == arm64 || $(arch) == aarch64 ]]; then
-    ARCH="arm64"
-elif [[ $(arch) == x86_64 ]]; then
-    ARCH="amd64"
-else
-    echo "Unable to determine CPU architecture. Only arm64/aarch64 and amd64/x86_64 are supported"
-    return
-fi
-echo "Detected ${ARCH} CPU architecture"
+    if [[ ${DISTRIB_CODENAME} == "jammy" || ${DISTRIB_CODENAME} == "focal" ]]; then
+        echo ${DISTRIB_CODENAME}
+    else
+        echo "${DISTRIB_CODENAME} is not supported by this installation script."
+        exit 1
+    fi
+    echo "Detected Ubuntu version ${DISTRIB_RELEASE}: ${DISTRIB_CODENAME}"
+
+    # Detect x86 cpu architecture or arm64 cpu architecture
+    if [[ $(arch) == arm64 || $(arch) == aarch64 ]]; then
+        ARCH="arm64"
+    elif [[ $(arch) == x86_64 ]]; then
+        ARCH="amd64"
+    else
+        echo "Unable to determine CPU architecture. Only arm64/aarch64 and amd64/x86_64 are supported"
+        exit 1
+    fi
+
+    echo "Detected ${ARCH} CPU architecture"
+}
 
 # Offer to uninstall ubuntu provided docker apt packages, if any
-packages="docker docker.io docker-compose docker-compose-v2 docker-doc podman-docker"
-echo "Do you wish to uninstall any legacy docker packages that may be installed? (Recommended)"
-echo "See https://docs.docker.com/engine/install/ubuntu/#prerequisites for more information"
-read -r -p "Remove ${packages} [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-     for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
-     do
-        sudo apt-get remove $pkg -y
-     done
-fi
+do_legacy_uninstall() {
+    packages="docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc"
+    echo "Do you wish to uninstall any legacy docker packages that may be installed? (Recommended)"
+    echo "See https://docs.docker.com/engine/install/ubuntu/#prerequisites for more information"
+    read -r -p "Remove ${packages} [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        sudo apt-get remove $packages -y
+    fi
+}
 
-# Install needed packages to add the official docker-ce apt repository
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+install_docker() {
+    # Install needed packages to add the official docker-ce apt repository
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-# Add and trust the official docker-ce apt repository
-echo "${DISTRIB_CODENAME}: Installing the gpg key for the docker-ce apt repository"
-if [ ${DISTRIB_CODENAME} == "jammy" ]; then
-    curl -s ${DOCKER_GPG} | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/docker.gpg --import
-    sudo chmod 644 /etc/apt/trusted.gpg.d/docker*
-elif [ ${DISTRIB_CODENAME} == "focal" ]; then
-   curl -fsSL ${DOCKER_GPG} | sudo apt-key add -
-fi
+    # Add and trust the official docker-ce apt repository
+    echo "${DISTRIB_CODENAME}: Installing the gpg key for the docker-ce apt repository"
+    if [ ${DISTRIB_CODENAME} == "jammy" ]; then
+        curl -s ${DOCKER_GPG} | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/docker.gpg --import
+        sudo chmod 644 /etc/apt/trusted.gpg.d/docker*
+    elif [ ${DISTRIB_CODENAME} == "focal" ]; then
+    curl -fsSL ${DOCKER_GPG} | sudo apt-key add -
+    fi
 
-# Add apt repository for the correct CPU archtecture and Ubuntu release version
-echo "Adding the docker-ce apt repository for CPU architecture ${ARCH} for Ubuntu ${DISTRIB_CODENAME}"
-sudo add-apt-repository "deb [arch=${ARCH}] ${DOCKER_APT} ${DISTRIB_CODENAME} stable" -y
+    # Add apt repository for the correct CPU archtecture and Ubuntu release version
+    echo "Adding the docker-ce apt repository for CPU architecture ${ARCH} for Ubuntu ${DISTRIB_CODENAME}"
+    sudo add-apt-repository "deb [arch=${ARCH}] ${DOCKER_APT} ${DISTRIB_CODENAME} stable" -y
 
-# Install the docker community edition (docker-ce)
-echo "Installing docker-ce (Community Edition)"
-sudo apt-get update
-sudo apt install docker-ce -y
+    # Install the docker community edition (docker-ce)
+    echo "Installing docker-ce (Community Edition)"
+    sudo apt-get update
+    sudo apt install docker-ce -y
+}
 
-# Repoprt the docker daemon status
-echo ""
-echo "Docker daemon status after installation"
-echo $(sudo systemctl status docker | grep 'Active')
-echo ""
+finalize_docker_install() {
+    # Repoprt the docker daemon status
+    echo ""
+    echo "Docker daemon status after installation"
+    echo $(sudo systemctl status docker | grep 'Active')
+    echo ""
 
-# Add the current user to the docker permissions group
-echo "Adding the current user ${USER} to the docker permissions group"
-sudo usermod -aG docker ${USER}
-echo "Done!"
-newgrp docker
+    # Add the current user to the docker permissions group
+    echo "Adding the current user ${USER} to the docker permissions group"
+    sudo usermod -aG docker ${USER}
+    echo "Done!"
+    newgrp docker
+}
+
+detect_os
+
+do_legacy_uninstall
+
+install_docker
+
+finalize_docker_install
